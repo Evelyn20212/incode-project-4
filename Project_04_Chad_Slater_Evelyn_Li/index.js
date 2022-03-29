d089d3a6e4f24342cb9cf1686956ca21d308fd23
Evelyn Li <evelyn@192-168-1-100.tpgi.com.au>
2022-03-29 16:10:41 +1100

    update index.js

--- Project_04_Chad_Slater_Evelyn_Li/index.js
+++ Project_04_Chad_Slater_Evelyn_Li/index.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-require("dotenv").config();
-
-const express = require("express");
-const path = require("path");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
-
-// Routers
-const loginRouter = require("./routes/login");
-app.use("/", loginRouter);
-
-const schedulesRouter = require("./routes/schedules");
-app.use("/schedules", schedulesRouter);
-
-const newSchedulesRouter = require("./routes/schedules-new");
-app.use("/new", newSchedulesRouter);
-
-const errorRouter = require("./routes/error");
-app.use("*", errorRouter);
-
-app.listen(PORT, () => {
-  console.log(`Schedule app listening at http://localhost:${PORT}/`);
-});
-=======
 require("dotenv").config();
 
 const express = require("express");
@@ -71,4 +39,3 @@ app.use("*", errorRouter);
 app.listen(PORT, () => {
   console.log(`Schedule app listening at http://localhost:${PORT}/`);
 });
->>>>>>> a99d3dc7cb6b48735af6563250ce7c66553b6c8f
