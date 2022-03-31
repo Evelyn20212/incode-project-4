c2c23f39fb7f2ef83e218dbdb16c03b3c3fbb0f3
Chad Slater <89958756+chad-slater@users.noreply.github.com>
2022-04-01 09:46:59 +1100

    Update protectedRoute.js

--- Project_04_Chad_Slater_Evelyn_Li/middleware/protectedRoute.js
+++ Project_04_Chad_Slater_Evelyn_Li/middleware/protectedRoute.js
@@ -1,5 +1,5 @@
 const protectedRoute = (req, res, next) => {
-  // user loggs in, then res.session.userId called
+  // user logs in, then res.session.userId called
   if (!req.session.userID) {
     res.clearCookie("mrcoffee_sid"); // from the front end
     res.status(401).redirect("/login");
@@ -11,7 +11,6 @@ const protectedRoute = (req, res, next) => {
 
 const successRoute = (req, res, next) => {
   if (req.session.userID) {
-    console.log("You are logged in, redirecting to home");
     res.redirect("/");
   } else {
     next();
