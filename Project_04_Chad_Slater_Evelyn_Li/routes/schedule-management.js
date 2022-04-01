aef9fd4c30d737b7ae9d57a6c01067659560d8c4
chad <slater.chad+incode@outlook.com>
2022-04-01 11:45:44 +1100

    change new route to schedule management and update. fix tfoot on schedules and schedule-management views for empty tables. update styles

--- Project_04_Chad_Slater_Evelyn_Li/routes/schedule-management.js
+++ Project_04_Chad_Slater_Evelyn_Li/routes/schedule-management.js
@@ -2,19 +2,33 @@ const express = require("express");
 const router = express.Router();
 
 const db = require("../database");
+const helper = require("../helper");
 const { protectedRoute } = require("../middleware/protectedRoute");
 
 // Get new schedule page
 // Post new schedule
 router
   .route("/")
-  .get("/", protectedRoute, (req, res) => {
-    res.render("schedule-management", {
-      title: "New Schedule",
-    });
+  .get(protectedRoute, (req, res) => {
+    db.any("SELECT * FROM schedule WHERE user_id = $1", [req.session.userID])
+      .then((schedule) => {
+        res.render("schedule-management", {
+          title: "Schedule Management",
+          schedule,
+          helper,
+        });
+      })
+      .catch((error) => {
+        res.render("error", {
+          title: "Error",
+          errorCode: 500,
+          errorMessage: error.message,
+        });
+      });
   })
   .post((req, res) => {
     const { day_of_week, start_time, end_time } = req.body;
+    const user_id = req.session.userID;
 
     db.none(
       "INSERT INTO schedule(user_id, day_of_week, start_time, end_time) VALUES($1, $2, $3, $4)",
