1b16750079d45ffd543b6112618962bcd27fe539
Chad Slater <89958756+chad-slater@users.noreply.github.com>
2022-04-01 19:42:06 +1100

    update login route, update seed_tables.sql

--- Project_04_Chad_Slater_Evelyn_Li/sql/seed_tables.sql
+++ Project_04_Chad_Slater_Evelyn_Li/sql/seed_tables.sql
@@ -1,10 +1,12 @@
 INSERT INTO
-   users(id,surname, firstname, email, password)
+   users(id, surname, firstname, email, password)
 VALUES
-('2cb8a9ee-ab20-11ec-b909-0242ac120002','Li','Eve','lihuaexample.lh@gmail.com','$2a$10$OBhpOhJ7gdAaq9e4hCApBe13LcQNwLZdvuWxDHqOzcZ2AKYAA3/du'),
-('2cb8a9ee-ab20-11ec-b909-0242ac120045','H','E','admin@gmail.com','admin');
+('aa88dba7-930e-4075-8405-3dd66434d5b2', 'john', 'doe', 'jd@email.com', '$2a$10$52i22ljiWwyfjfhMVOvnceI.hMNhgHcXEfBsNvr8yFp38ibZbTESa'),
+('91bda5be-e6c5-4a6d-931a-357147d62c7c','admin','admin','admin@email.com','$2a$10$82nRc1ZlUIyrVLCZkHjOp.f5h4kHYLtwkBvCl1oq1.ft/NV0IXmvm');
 INSERT INTO
    schedule(user_id, day_of_week, start_time, end_time)
 VALUES
-('2cb8a9ee-ab20-11ec-b909-0242ac120002',4,'14:00', '16:00');
+('aa88dba7-930e-4075-8405-3dd66434d5b2', 1, '00:00', '01:00'),
+('aa88dba7-930e-4075-8405-3dd66434d5b2', 2, '00:01', '02:00'),
+('91bda5be-e6c5-4a6d-931a-357147d62c7c', 1, '02:00', '03:00');
 
